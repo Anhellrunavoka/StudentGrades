dc673e1acf49f06a86a490ce5fef560d7359cd06
Anhellrunavoka <angelina.gordeeva10@gmail.com>
2024-10-15 21:06:48 +0300

    Додані нові функції  і також виправленні деякі баги та помилки

--- Student.cpp
+++ Student.cpp
@@ -8,21 +8,31 @@ const char* Student::getName()
     return name;
 }
 
-Student::Student(const char* studentName, const char* studentBirth, const char* studentPhone){
+Student::Student(const char* studentName, const char* studentBirth, const char* studentPhone, const char* StudentTown, const char* StudentCountry, const char* scho, int numn){
+    name = new char[20];
+    birth = new char[20];
+    phone = new char[15];
+    town = new char[50];
+    country = new char[50];
+    school = new char[50];
     strcpy_s(name, 20, studentName);
     strcpy_s(birth, 20, studentBirth);
     strcpy_s(phone, 15, studentPhone);
+    strcpy_s(town, 20, StudentTown);
+    strcpy_s(country, 20, StudentCountry);
+    strcpy_s(school, 15, scho);
+    this->number_group = numn;
 }
-void Student::SetTownCountrySch() {
-    cout << "Input town:"<<endl;
-    cin >> town;
-    cout << "Input country:" << endl;
-    cin >> country;
-    cout << "Input school:" << endl;
-    cin >> school;
-    cout << "Input group's number:" << endl;
-    cin >> number_group;
+void Student::Print() {
+    cout << endl << (name ? name : "[No Name]") << " : "
+        << (birth ? birth : "[No Birth]") << " , "
+        << (phone ? phone : "[No Phone]") << " , "
+        << (town ? town : "[No Town]") << " , "
+        << (country ? country : "[No Country]") << " , "
+        << (school ? school : "[No School]") << " , "
+        << number_group << endl;
 }
+
 Student::Student( Student&& ob):name{ ob.name }, birth{ ob.birth }, phone{ ob.phone }, town{ ob.town }, country{ ob.country }, school{ ob.school }, number_group{ ob.number_group } {
         ob.name = nullptr;
         ob.birth = nullptr;
@@ -35,13 +45,13 @@ Student::Student( Student&& ob):name{ ob.name }, birth{ ob.birth }, phone{ ob.ph
 }
 Student& Student::operator=(Student&& ob)
 {
-    if (!(this == &ob)) {
-        delete name;
-        delete birth;
-        delete phone;
-        delete town;
-        delete country;
-        delete school;
+    if (this != &ob) {
+        delete[] name;
+        delete[] birth;
+        delete[] phone;
+        delete[] town;
+        delete[] country;
+        delete[] school;
         name = ob.name;
         birth = ob.birth;
         phone = ob.phone;
@@ -60,12 +70,27 @@ Student& Student::operator=(Student&& ob)
     }
     return *this;
 }
-void Student::Print() {
-    cout << endl << name << " : " << birth << " , " << phone << " , " << town << " , " << country << " , " << school << " , " << number_group<<endl;
+
+string Student::Study() {
+    return  "The student is studying in general program.";
 }
-Aspirant::Aspirant(const char* studentName, const char* studentBirth, const char* studentPhone, string tematika, string dat)
-    :Student(studentName, studentBirth, studentPhone), tema(tematika), data(dat) {}
+Student::~Student() {
+    delete[] name;
+    delete[] birth;
+    delete[] phone;
+    delete[] town;
+    delete[] country;
+    delete[] school;
+}
+Aspirant::Aspirant(const char* studentName, const char* studentBirth, const char* studentPhone, const char* StudentTown, const char* StudentCountry, const char* school, int numn, string tematika, string dat)
+    :Student(studentName, studentBirth, studentPhone, StudentTown, StudentCountry, school,numn), tema(tematika), data(dat) {}
+
 void Aspirant::Print() {
     Student::Print();
     cout << endl << "Tema: " << tema << "\nData: " << data;
+}
+
+string Aspirant::Study(string Napravlen){
+    this->Napravlennya = Napravlen;
+    return "The student is studying in "+Napravlennya +" faculty.";
 }
--- Student.h
+++ Student.h
@@ -13,17 +13,20 @@ private://public://help zminuvati na pryamu modifire(privat,protecyed(dochirni c
     int number_group;
 public:
     const char* getName();
-    Student(const char* studentName, const char* studentBirth, const char* studentPhone);
-    void SetTownCountrySch();
+    Student(const char* studentName, const char* studentBirth, const char* studentPhone, const char* StudentTown, const char* StudentCountry, const char* school,int numn);
     void Print();
     Student( Student&& ob);
     Student& operator=(Student&& ob);
+    string Study();
+    ~Student();
 };
 class Aspirant :public Student {
     string tema;
     string data;
+    string Napravlennya;
 public:
-     Aspirant(const char* studentName, const char* studentBirth, const char* studentPhone,string tematika,string dat);
+     Aspirant(const char* studentName, const char* studentBirth, const char* studentPhone, const char* StudentTown, const char* StudentCountry, const char* school, int numn,string tematika,string dat);
      void Print();
+     string Study(string Napravlen);
 };
 
